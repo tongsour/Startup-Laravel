2be31b141522870b892ca43f04dc4a7f87661d3b
tongsour <tongsour1000@gmail.com>
2025-03-04 22:16:10 +0700

    fix bug with image

--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
-    //
-    protected $fillable = ['name'];  
+    protected $fillable = ['name', 'description', 'pricing', 'category_id', 'images'];
+
+    protected $casts = [
+        'images' => 'array'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
